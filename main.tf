09a759f3eea1e34a95dba99c02fbfc4dfa9ad150
anand nandhu <anandnandhu52@gmail.com>
2023-04-24 14:10:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
     {name = "${var.env}-vpc"}
     )
 }
+
+resource "aws_subnet" "main" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.cidr_block
+  tags = merge(
+    local.common_tags,
+    { name = "${var.env}-subnet" }
+    )
+}
