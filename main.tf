133de0fe2edd5db034f8b9967a1d9f41958571d1
anand nandhu <anandnandhu52@gmail.com>
2023-04-26 12:34:14 +0530

    terraform

--- main.tf
+++ main.tf
@@ -67,6 +67,12 @@ resource "aws_route_table" "public" {
   )
 }
 
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
 #create ec2
 
 #data "aws_ami" "centos8" {
