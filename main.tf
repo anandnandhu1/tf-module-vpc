a4ffef7203e0831cd756a3af1060376301916fe8
anand nandhu <anandnandhu52@gmail.com>
2023-04-26 11:55:34 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,28 +1,30 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  tags       = merge(
+  tags = merge(
     local.common_tags,
-    {Name = "${var.env}-vpc"}
-    )
+    { Name = "${var.env}-vpc" }
+  )
 }
 
 resource "aws_subnet" "public" {
-  count         = length(var.public_subnets_cidr)
-  vpc_id        = aws_vpc.main.id
-  cidr_block    = var.public_subnets_cidr[count.index]
+  count      = length(var.public_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[count.index]
+
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-public-subnet-${count.index+1}" }
-    )
+    { Name = "${var.env}-public-subnet-${count.index + 1}" }
+  )
 }
 
 resource "aws_subnet" "private" {
-  count         = length(var.private_subnets_cidr)
-  vpc_id        = aws_vpc.main.id
-  cidr_block    = var.private_subnets_cidr[count.index]
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-private-subnet-${count.index+1}" }
+    { Name = "${var.env}-private-subnet-${count.index + 1}" }
   )
 }
 
@@ -30,15 +32,12 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
-  auto_accept = true
+  auto_accept   = true
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-peering" }
   )
-
 }
-
-
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -48,7 +47,6 @@ resource "aws_internet_gateway" "igw" {
     { Name = "${var.env}-igw" }
   )
 }
-
 #create ec2
 
 #data "aws_ami" "centos8" {
