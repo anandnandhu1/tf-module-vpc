452286f461fc9b0864c8de385d9014e3ef461699
anand nandhu <anandnandhu52@gmail.com>
2023-04-26 10:36:04 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  tags = merge(
+  tags       = merge(
     local.common_tags,
     {Name = "${var.env}-vpc"}
     )
 }
 
 resource "aws_subnet" "main" {
-  count = length(var.subnets_cidr)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.subnets_cidr[count.index]
+  count         = length(var.subnets_cidr)
+  vpc_id        = aws_vpc.main.id
+  cidr_block    = var.subnets_cidr[count.index]
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-subnet-${count.index+1}" }
@@ -29,8 +29,8 @@ resource "aws_vpc_peering_connection" "peer" {
 }
 
 resource "aws_route" "default" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  destination_cidr_block = "172.31.0.0/16"
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
@@ -59,10 +59,10 @@ data "aws_ami" "centos8" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id = aws_subnet.main.*.id[0]
+  subnet_id              = aws_subnet.main.*.id[0]
 
   tags = {
     Name = "anand"
