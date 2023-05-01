a533262dd9ae4a1ce9bdb12ce5d9d1eca312f66f
anand nandhu <anandnandhu52@gmail.com>
2023-05-01 14:14:41 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,5 +1,6 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "availability_zone" {}
